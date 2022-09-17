dca7f18e5e44b9543464f62b72db770cbb7d65d4
xiaobai <xiaobai@trip.com>
2022-09-18 00:45:40 +0800

    add new message

--- PrintMessage.java
+++ PrintMessage.java
@@ -6,5 +6,6 @@ public class PrintMessage {
         System.out.println("ccc");
         System.out.println("ddd");
         System.out.println("fffff");
+        System.out.println("ggggg");
     }
 }
