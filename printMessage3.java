7909ed5c1fa31a06999080e8df61b094ea3c86e7
xiaobai <xiaobai@trip.com>
2022-09-18 00:19:46 +0800

    create new printMessage class

---
+++ printMessage3.java
@@ -0,0 +1,9 @@
+public class printMessage3 {
+
+    public static void main(String[] args) {
+        System.out.println("aaa");
+        System.out.println("bbb");
+        System.out.println("ccc");
+        System.out.println("ddd");
+    }
+}
