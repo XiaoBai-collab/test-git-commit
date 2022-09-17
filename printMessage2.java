3ba5d1efbbedea148548b6305930703a92adee7b
xiaobai <xiaobai@trip.com>
2022-09-18 00:40:07 +0800

    delete printMessage2

--- printMessage2.java
+++
@@ -1,9 +0,0 @@
-public class printMessage2 {
-
-    public static void main(String[] args) {
-        System.out.println("aaa");
-        System.out.println("bbb");
-        System.out.println("ccc");
-        System.out.println("ddd");
-    }
-}
