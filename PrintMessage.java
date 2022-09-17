c07010646c91d286bb11f897faec71c0f6bfc991
xiaobai <xiaobai@trip.com>
2022-09-17 22:43:57 +0800

    print other message

--- PrintMessage.java
+++ PrintMessage.java
@@ -2,5 +2,7 @@ public class PrintMessage {
 
     public static void main(String[] args) {
         System.out.println("aaa");
+        System.out.println("bbb");
+        System.out.println("ccc");
     }
 }
