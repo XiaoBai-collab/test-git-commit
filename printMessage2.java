9c23a3f8410f54faa17bb0bcf2c131caa9ccb649
xiaobai <1366151498@qq.com>
2022-09-18 10:14:31 +0800

    create new printMessage class

---
+++ printMessage2.java
@@ -0,0 +1,9 @@
+public class printMessage2 {
+
+    public static void main(String[] args) {
+        System.out.println("aaa");
+        System.out.println("bbb");
+        System.out.println("ccc");
+        System.out.println("ddd");
+    }
+}
